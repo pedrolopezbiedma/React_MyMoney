233d5d70e17b561517ae2d4a166d359e84800849
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-26 11:04:07 +0100

    Including the Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import Navbar from './components/Navbar'
 
 function App() {
   return <div className="App">
     <BrowserRouter>
+    <Navbar />
       <Switch>
         <Route exact path='/'>
           <Home />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+// React
+import { Link } from 'react-router-dom'
+
+// Styles
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return <nav className={styles.navbar}>
+    <ul>
+        <li className={styles.title}>My Money</li>
+
+        <li><Link to='/signup'>Signup</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+    </ul>
+  </nav>;
+};
+
+export default Navbar;
