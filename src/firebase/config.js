36c0994c95f66b6535a35d9956a5e45bcd4b8635
Pedro Lopez Biedma <plo@novatec-gmbh.de>
2024-01-26 11:43:35 +0100

    Set up Firebase & Firebase Authentication

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBD_7UaDl6_tZgEVIoy0kLZSVkCvTZ3IbY",
+  authDomain: "react-my-money-59a73.firebaseapp.com",
+  projectId: "react-my-money-59a73",
+  storageBucket: "react-my-money-59a73.appspot.com",
+  messagingSenderId: "221448545677",
+  appId: "1:221448545677:web:52bdb967bb6211aa13c9c4",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectAuthentication = firebase.auth();
+
+// exports
+export { projectAuthentication };
